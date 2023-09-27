b8a4063b3be4706e8d1920cc6959599d649e0199
6395ashish <6395ashish@gmail.com>
2023-09-27 17:29:27 +0000

    initial commit

---
+++ RandomStringGenerator.java
@@ -0,0 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class RandomStringGenerator {
+	public static void main(String[] args) {
+
+}
+}
+
+
